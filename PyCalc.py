6e20333e933f264201f317b24063b9647da68678
ashan1979 <indrajee79@gmail.com>
2026-02-18 13:17:57 +0530

    Simple Calculator in TK

---
+++ PyCalc.py
@@ -0,0 +1,54 @@
+import tkinter as tk
+
+def on_click(button_text):
+    current_text = entry.get()
+
+    if button_text == "=":
+        try:
+            # calculate the result
+            result = eval(current_text)
+            entry.delete(0, tk.END)
+            entry.insert(tk.END, str(result))
+
+        except Exception:
+            entry.delete(0, tk.END)
+            entry.insert(tk.END, "Error")
+
+    elif button_text == "C":
+        entry.delete(0, tk.END)
+
+    else:
+        entry.insert(tk.END, button_text)
+
+# Initialize window
+root = tk.Tk()
+root.title("Basic Calc")
+root.geometry("300x400")
+
+# Display screen
+entry = tk.Entry(root, font=("Arial", 24), borderwidth=5, relief="flat", justify="right")
+entry.grid(row=0, column=0, columnspan=4, padx=10, pady=20)
+
+# Button Labels
+
+buttons = [
+    '7', '8', '9', '/',
+    '4', '5', '6', '*',
+    '1', '2', '3', '-',
+    'C', '0', '=', '+'
+]
+
+# Create and place buttons
+row_val = 1
+col_val = 0
+
+for button in buttons:
+    action = lambda x=button: on_click(x)
+    tk.Button(root, text=button, width=5, height=2, font=('Arial', 14),
+              command=action).grid(row=row_val, column=col_val,padx=5, pady=5)
+    col_val += 1
+    if col_val > 3:
+        col_val = 0
+        row_val += 1
+
+root.mainloop()
