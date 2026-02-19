ead1989e314d8e977e4163baaed6c35f1a6c3347
ashan1979 <indrajee79@gmail.com>
2026-02-19 10:48:34 +0530

    Asterisk Box

---
+++ inputoutput.py
@@ -0,0 +1,7 @@
+name_var = input(print("what's your name?"))
+age_var = input(print("what's your age?"))
+marital_var = input(print("what's your marital status?"))
+
+print("Your name is " + name_var)
+print("Your age is " + age_var)
+print("Your marital status is " + marital_var)
