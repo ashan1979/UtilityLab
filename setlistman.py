8216443b9453185fcff5578c2131b729d3bdb533
ashan1979 <indrajee79@gmail.com>
2026-02-24 16:52:08 +0530

    Very simple setlist tool to demo how append works.

---
+++ setlistman.py
@@ -0,0 +1,10 @@
+setlist = []
+
+setlist.append("Comfortably numb")
+setlist.append("Highway to hell")
+setlist.append("Master master of puppets")
+
+print(f"Tonight's setlist is: {setlist}")
+
+number_of_songs = len(setlist)
+print(f"Number of songs in your setlist is: {number_of_songs}")
